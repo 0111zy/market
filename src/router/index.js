880979836c5efcab5b704f2454b2cbeb354ab357
ssf <2293210135@qq.cpm>
2022-04-01 22:58:20 +0800

    修改目录结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 
-const Home = () => import('../views/home/Home.vue')
-const Category = () => import('../views/category/Category.vue')
-const Cart = () => import('../views/cart/Cart.vue')
-const Profile = () => import('../views/profile/Profile.vue')
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Cart = () => import('../views/cart/Cart')
+const Profile = () => import('../views/profile/Profile')
 
-Vue.use(Router)
+// 1.安装插件
+Vue.use(VueRouter)
 
+// 2.创建router
 const routes = [
   {
     path: '',
@@ -20,18 +22,20 @@ const routes = [
   {
     path: '/category',
     component: Category
-  },,
+  },
   {
     path: '/cart',
     component: Cart
-  },,
+  },
   {
     path: '/profile',
     component: Profile
-  },
+  }
 ]
-
-export default new Router({
+const router = new VueRouter({
   routes,
   mode: 'history'
 })
+
+
+export default router
