3422f06cc0b63e1cb1f00f435950eb92c3c94444
ssf <2293210135@qq.cpm>
2022-04-07 22:27:30 +0800

    tabControl封装(切换tab)

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,15 +4,18 @@
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
     <feature-view/>
+    <tab-control class="tab-cobtrol" :titles="['流行','新款','精选']"/>
   </div>
 </template>
 
 <script>
+  // 公共组件
   import NavBar from 'components/common/navbar/NavBar';
+  import TabControl from 'components/content/tabControl/TabControl';
+  // 子组件
   import HomeSwiper from './childComps/HomeSwiper'
   import RecommendView from './childComps/RecommendView'
   import FeatureView from './childComps/FeatureView'
-
 
   // 获取接口数据
   import {getHomeMultidata} from "network/home";
@@ -21,6 +24,7 @@
     name: "Home",
     components: {
       NavBar,
+      TabControl,
       HomeSwiper,
       RecommendView,
       FeatureView
@@ -57,5 +61,11 @@
     right: 0;
     top: 0;
     z-index: 9 
+  }
+  .tab-cobtrol{
+    /* 设置了sticky的元素，在屏幕范围（viewport）时该元素的位置并不受到定位影响（设置是top、left等属性无效），
+       当该元素的位置将要移出偏移范围时，定位又会变成fixed，根据设置的left、top等属性成固定位置的效果 */
+    position: sticky;   
+    top: 44px;
   }
 </style>
